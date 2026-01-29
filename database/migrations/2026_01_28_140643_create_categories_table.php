405d169dd5bb460abf9044c5227330b6b7579d0b
Sami-Regragui-Work <sami-regragui-work@protonmail.com>
2026-01-29 12:29:36 +0100

    feat(migrations): added 4 tables columns and migrated

--- database/migrations/2026_01_28_140643_create_categories_table.php
+++ database/migrations/2026_01_28_140643_create_categories_table.php
@@ -13,6 +13,10 @@ return new class extends Migration
     {
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
+            $table->string('name', 255);
+            $table->string('slug', 255)->unique();
+            $table->text('description')->nullable();
+            $table->softDeletes();
             $table->timestamps();
         });
     }
